e740f97fe63c1707c5f4468a822846848efbed42
madhuracodes <madhura84@gmail.com>
2024-10-29 10:17:31 +0100

    tweaked css and added JS

--- src/index.js
+++ src/index.js
@@ -14,8 +14,11 @@ function generateStory(event) {
   let apiKey = "40912tb49034f3e73f11616ed1a9396o";
   let prompt = `User instruction: Generate a short story about ${userInstrtuctions.value}`;
   let context =
-    "You are a smart AI who knows many stories. your mission is to generate a 12 line short story. Make sure to follow the user instructions.";
+    "You are a smart AI who knows many stories. your mission is to generate a 12 line short story with a title in basic HTML. Please do not mention html in your answer. Make sure to follow the user instructions. Sign the story with 'Maddy's AI' inside a <strong> element at the END of the story and separate it with a </br>.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let storyElemnt = document.querySelector("#story");
+  storyElemnt.classList.remove("hidden");
 
   axios.get(apiUrl).then(displayStory);
 }
